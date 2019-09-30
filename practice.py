c5ea99b62f198549d654c0b28d31ab82d508cb9e
100127902 <alexviner2016@gmail.com>
2019-09-30 09:01:29 -0500

    chess

---
+++ practice.py
@@ -0,0 +1,14 @@
+# this is an octothorpe -- it makes a comment (line ignored by the computer)
+# print("hello world")
+print("Howdy, ya'll")
+print("I like typing this.")
+print("this is fun.")
+
+# math skills demo
+
+print("I will not count my chickens", 2)
+print("Hens: ", 25+30/6)
+print("Roosters: ", 100 - 25 * 3 % 4)
+print("Now I will count the eggs", 7)
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+print(3 + 2 < 5 - 7)
