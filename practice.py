19918ad27fc153dfc9aac60bb9ccb3a5081fb582
100127902 <alexviner2016@gmail.com>
2019-10-01 08:47:46 -0500

    chess

--- practice.py
+++ practice.py
@@ -1,15 +1,32 @@
 # this is an octothorpe -- it makes a comment (line ignored by the computer)
+
 # print("hello world")
 print("Howdy, ya'll")
 # Just added a welcoming text to the code
 print("I like typing this.")
 print("this is fun.")
 
-# math skills demo
+# Blacks math skill demo
 
 print("I will now count my chickens", 2)
+# added number to text
 print("Hens: ", 25+30/6)
+# PEMDAS mathmatical solution
 print("Roosters: ", 100 - 25 * 3 % 4)
+# Pemdas, multiply to get -75 then 4% just means that 4 fits into 75 18 times to make 72 which leaves
+# you with a left over number of 3 that you now subtract from 100 to get 97.
 print("Now I will count the eggs", 7)
 print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
 print(3 + 2 < 5 - 7)
+
+# My math skill demo
+
+print("We have too much men for one bottle of russian vodka")
+print("Fourty two men, 50 ounces of russian vodka, how do we split?")
+print(50 % 42)
+print("Very good each man gets one ounce, Now how do we split the rest of the 8 ounces?")
+print("Big problem we do not have a devisible amount, what do we do now?")
+print("Subtract men")
+print("how do we subtract man? Like dis")
+print(42 % 8)
+print("Now that is number of men we must decease")
